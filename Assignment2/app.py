21a0248367af9b0adfa9225a5710621596e79cd7
LISIMEI <lsmdlut@gmail.com>
2022-04-12 15:33:37 +0200

    login template

--- Assignment2/app.py
+++ Assignment2/app.py
@@ -7,16 +7,19 @@ from hashids import Hashids
 app = Flask(__name__)
 
 hashids = Hashids()
-URLS={}
+URLS = {}
 
 
 id = 0
+
+
 def get_key(val):
     for key, value in URLS.items():
-         if val == value:
-             return key
- 
+        if val == value:
+            return key
+
     return "key doesn't exist"
+
 
 def isValidURL(str):
     regex = ("((http|https)://)(www.)?" +
@@ -32,69 +35,87 @@ def isValidURL(str):
     else:
         return False
 
-#show all the URLs
-@app.route('/URLList', methods=('GET','POST'))
+# show all the URLs
+
+
+@app.route('/URLList', methods=('GET', 'POST'))
 def showUrls():
     if request.method == 'GET':
-        return render_template('list.html', urls=URLS )
-#shorten url to original url
-@app.route('/<string:hashid>/',methods=('GET','POST'))
+        return render_template('list.html', urls=URLS)
+# shorten url to original url
+
+
+@app.route('/<string:hashid>/', methods=('GET', 'POST'))
 def redirectURl(hashid):
     myurl = request.host_url + hashid
     if hashid in URLS:
         return redirect(URLS[hashid])
     else:
         listurl = request.host_url+'URLList'
-        return render_template('404.html', home_url=request.host_url ,list_url=listurl), 404
-#update url
-@app.route('/update/<string:hashid>',methods=('GET', 'POST'))
+        return render_template('404.html', home_url=request.host_url, list_url=listurl), 404
+# update url
+
+
+@app.route('/update/<string:hashid>', methods=('GET', 'POST'))
 def updateURl(hashid):
     if request.method == 'GET':
-        return render_template('index.html', url_false = False)
+        return render_template('index.html', url_false=False)
     if request.method == 'POST':
-        url= request.form['url']
+        url = request.form['url']
 
         if isValidURL(url) == False:
             shorturl = ''
-            return render_template('index.html', url_false = True)
+            return render_template('index.html', url_false=True)
         else:
             if url in URLS.values():
-                key=get_key(url)
+                key = get_key(url)
                 del URLS[key]
             URLS[hashid] = url
             shorturl = request.host_url + hashid
             return render_template('index.html', short_url=shorturl)
-#delete url
-@app.route('/delete/<string:hashid>',methods=['GET'])
+# delete url
+
+
+@app.route('/delete/<string:hashid>', methods=['GET'])
 def deleteURl(hashid):
     del URLS[hashid]
-    return render_template('list.html', urls=URLS )
+    return render_template('list.html', urls=URLS)
 
-#delete all
-@app.route('/delete/All',methods=['GET'])
+# delete all
+
+
+@app.route('/delete/All', methods=['GET'])
 def deleteAllURl():
-    URLS={}
+    URLS = {}
     id = 0
     return "<p>Database is empty.</p>"
-#default
+# default
+
+
 @app.route('/', methods=('GET', 'POST'))
 def createShortUrl():
     global URL
     if request.method == 'POST':
         global id
-        url= request.form['url']
+        url = request.form['url']
 
         if isValidURL(url) == False:
             shorturl = ''
-            return render_template('index.html', url_false = True)
+            return render_template('index.html', url_false=True)
         else:
             id = id+1
             hashid = hashids.encode(id)
             if url in URLS.values():
-                key=get_key(url)
+                key = get_key(url)
                 del URLS[key]
             URLS[hashid] = url
             shorturl = request.host_url + hashid
             return render_template('index.html', short_url=shorturl)
     return render_template('index.html')
 
+# login url
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
